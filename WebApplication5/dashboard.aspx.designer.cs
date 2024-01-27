4ab5c1238e278cae81a452ae8e336999f21c9509
evner <evner_blanco@hotmail.com>
2024-01-27 09:46:55 -0600

    e

--- WebApplication5/dashboard.aspx.designer.cs
+++ WebApplication5/dashboard.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace WebApplication5
 
     public partial class dashboard
     {
+
+        /// <summary>
+        /// sidebarliteral control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal sidebarliteral;
     }
 }
